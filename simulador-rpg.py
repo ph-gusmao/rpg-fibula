30921dc651fb4146648236454c1d36224b3d341a
ph-gusmao <fox_max_power@outlook.com>
2026-07-16 00:39:15 -0300

    feat:adicionando título

---
+++ simulador-rpg.py
@@ -0,0 +1,16 @@
+import random
+
+
+def mostrar_titulo():
+    print("===================================")
+    print("      SIMULADOR DE RPG EM TEXTO     ")
+    print("===================================")
+
+
+def iniciar_jogo():
+    nome = input("Digite o seu nome: ")
+    print(f"Seja Bem-vindo, {nome}!")
+
+
+mostrar_titulo()
+iniciar_jogo()
