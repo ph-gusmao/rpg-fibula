e02f0009257ad5dc50a6bceac9b917d0c165e883
ph-gusmao <fox_max_power@outlook.com>
2026-07-16 13:25:46 -0300

    feat: adicionando monstros

--- simulador-rpg.py
+++ simulador-rpg.py
@@ -19,5 +19,16 @@ def iniciar_jogo():
     return nome, hp, lvl, forca, exp, inventario, status
 
 
+def sortear_monstro(jogador_lvl):
+    # Lista de monstros: [nome, hp, força, exp]
+    slime = ["Slime", 10, 2, 10]
+    goblin = ["Goblin", 20, 4, 20]
+    troll = ["Troll", 40, 8, 40]
+    orc = ["Orc", 80, 16, 80]
+    mumia = ["Múmia", 160, 32, 160]
+    quimera = ["Quimera", 320, 64, 320]
+    dragao = ["Dragão", 1000, 100, 1000]
+
+
 mostrar_titulo()
 nome, hp, lvl, forca, exp, inventario, status = iniciar_jogo()
