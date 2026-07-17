c4ba754ecaee9abf7269d1098d9eaf2d4f500f96
ph-gusmao <fox_max_power@outlook.com>
2026-07-17 00:26:29 -0300

    feat:finalizando o loop do jogo

--- simulador-rpg.py
+++ simulador-rpg.py
@@ -67,7 +67,7 @@ def iniciar_jogo():
     inventario = []
     status = True  # Vivo(True) ou morto(False)
 
-    return nome, lvl, exp, inventario, status, hp, forca
+    return nome, hp, lvl, forca, exp, inventario, status
 
 
 def escolher_raca():
@@ -97,13 +97,18 @@ def sortear_monstro(jogador_lvl):
     # dragao = ["Dragão", 1000, 100, 1000]
 
     monstros = {
-        "slime": ["Slime", 10, 2, 10],
-        "goblin": ["Goblin", 20, 4, 20],
-        "troll": ["Troll", 40, 8, 40],
-        "orc": ["Orc", 80, 16, 80],
-        "mumia": ["Múmia", 160, 32, 160],
-        "quimera": ["Quimera", 320, 64, 320],
-        "dragao": ["Dragão", 1000, 100, 1000],
+        "slime": {
+            "monstro_nome": "slime",
+            "monstro_hp": 10,
+            "monstro_forca": 10,
+            "monstro_exp": 10,
+        },
+        "goblin": {
+            "monstro_nome": "Goblin",
+            "monstro_hp": 20,
+            "monstro_exp": 20,
+            "monstro_forca": 20,
+        },
     }
 
     # Retornando monstros de acordo com lvl do jogador
@@ -153,8 +158,8 @@ def atacar(atacante_nome, atacante_forca, defensor_nome, defensor_hp, defensor_f
 
 
 # calcular o level up
-def calcular_lvl(jogador_lvl, jogador_exp, jogador_hp, jogador_forca, exp_monstro):
-    jogador_exp = jogador_exp + exp_monstro
+def calcular_lvl(jogador_lvl, jogador_exp, jogador_hp, jogador_forca, monstro_exp):
+    jogador_exp = jogador_exp + monstro_exp
     exp_necessaria = jogador_lvl**3
 
     if jogador_exp >= exp_necessaria:
@@ -228,10 +233,68 @@ while True:
         monstro_nome, monstro_hp, monstro_forca, monstro_exp = sortear_monstro(
             jogador_lvl
         )
-        print(f"Um {monstro_nome} apareceu! (HP: {monstro_hp}, Força: {monstro_forca})")
+        print(
+            f"Um {monstro_nome} aleatório apareceu!\n HP: {monstro_hp}\n Força: {monstro_forca}"
+        )
         jogador_lutando = True
+    else:
+        print(f"Você está enfrentando um {monstro_nome}!\n HP: {monstro_hp}\n")
 
+        print(f"{jogador_nome} (HP: {jogador_hp}/100, Força: {jogador_forca})")
+        print(f"Level: {jogador_lvl}")
+        print(f"O que você deseja fazer?")
+        print("1. Atacar")
+        print("2. Usar item")
+        print("3. Fugir")
+        print("4. Visualizar status")
+        print("5. Sair do jogo")
+        opcao = int(input("Escolha uma opção: "))
 
-raca, hp, forca = escolher_raca()
-monstro_sorteado = sortear_monstro(lvl)
-print(f"{monstro_sorteado}")
+        if opcao == 1:
+            monstro_hp = atacar(
+                jogador_nome, jogador_forca, monstro_nome, monstro_hp, monstro_forca
+            )
+            print("\n")
+
+            if monstro_hp > 0:
+                jogador_hp = atacar(
+                    monstro_nome, monstro_forca, jogador_nome, jogador_hp, jogador_forca
+                )
+                print("\n")
+
+                if jogador_hp <= 0:
+                    game_over()
+            else:
+                print(f"Você ganhou {monstro_exp} de XP!")
+                jogador_lvl, jogador_exp, jogador_hp, jogador_forca = calcular_lvl(
+                    jogador_lvl, jogador_exp, jogador_hp, jogador_forca, monstro_exp
+                )
+                pocao = obter_pocao()
+                if pocao is not None:
+                    jogador_inventario.append(pocao)
+                jogador_lutando = False
+        elif opcao == 2:
+            jogador_hp, jogador_inventario = usar_item(jogador_inventario, jogador_hp)
+        elif opcao == 3:
+            fugiu = fugir(jogador_nome)
+            if fugiu:
+                jogador_lutando = False
+            else:
+                jogador_hp = atacar(
+                    monstro_nome, monstro_forca, jogador_nome, jogador_hp, jogador_forca
+                )
+                print("\n")
+                if jogador_hp == 0:
+                    game_over()
+        elif opcao == 4:
+            print(f"\n{jogador_nome}")
+            print(f"\nHP: {jogador_hp}/100")
+            print(f"\nLVL: {jogador_lvl}")
+            print(f"\nEXP: {jogador_exp}")
+            exp_proximo_lvl = jogador_exp**3
+            print(f"\nVocê precisa de {exp_proximo_lvl} XP para subir de nível")
+            print(f"\nForça: {jogador_forca}")
+            print(f"\nInventário: {jogador_inventario}\n")
+
+        elif opcao == 5:
+            game_over()
